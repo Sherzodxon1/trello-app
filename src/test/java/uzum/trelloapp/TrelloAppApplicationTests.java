8e9f707fcb1a82086e70d9e03a75e49eee820560
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-21 16:46:06 +0500

    1 commit

---
+++ src/test/java/uzum/trelloapp/TrelloAppApplicationTests.java
@@ -0,0 +1,13 @@
+package uzum.trelloapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrelloAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
