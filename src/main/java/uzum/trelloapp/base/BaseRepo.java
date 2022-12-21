1bd6dbf5faaddcb30aba6c25bd08b715a42d601d
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-21 17:04:49 +0500

    1) Bases 2) DateTime converter 3) ResponseData 4) Utils added

---
+++ src/main/java/uzum/trelloapp/base/BaseRepo.java
@@ -0,0 +1,4 @@
+package uzum.trelloapp.base;
+
+public class BaseRepo {
+}
---
+++ src/main/java/uzum/trelloapp/helper/Utils.java
@@ -0,0 +1,56 @@
+package uzum.trelloapp.helper;
+
+import org.springframework.util.StringUtils;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+public class Utils {
+
+    public static final DateTimeFormatter formatLocalDateTime = DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm:ss");
+    public static final DateTimeFormatter formatLocal = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+
+    public static LocalDateTime toLocalDateTime(String stringTime) {
+        return LocalDateTime.parse(stringTime, formatLocalDateTime);
+    }
+
+    public static LocalDate toLocalDate(String stringDate) {
+        return LocalDate.parse(stringDate, formatLocal);
+    }
+
+    public static boolean isEmpty(Object obj) {
+        return obj == null;
+    }
+
+    public static boolean isEmpty(String str) {
+        return !StringUtils.hasText(str);
+    }
+
+    public static boolean isEmpty(List<?> list) {
+        return list == null || list.isEmpty();
+    }
+
+    public static boolean isEmpty(Map<?, ?> map) {
+        return map == null || map.isEmpty();
+    }
+
+    public static boolean isPresent(Object obj) {
+        return obj != null;
+    }
+
+    public static boolean isPresent(String str) {
+        return StringUtils.hasText(str);
+    }
+
+    public static boolean isPresent(List<?> list) {
+        return list != null && !list.isEmpty();
+    }
+
+    public static boolean isPresent(Map<?, ?> map) {
+        return map != null && !map.isEmpty();
+    }
+
+}
