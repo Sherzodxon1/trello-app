ac906106fdfd30ebd43627ae3ec29b533ef16b14
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-21 17:46:01 +0500

    Role added

--- src/main/java/uzum/trelloapp/base/BaseRepo.java
+++ src/main/java/uzum/trelloapp/base/BaseRepo.java
@@ -1,4 +1,8 @@
 package uzum.trelloapp.base;
 
-public class BaseRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
+
+@NoRepositoryBean
+public interface BaseRepo<E> extends JpaRepository<E, Long> {
 }
---
+++ src/main/java/uzum/trelloapp/entity/Role.java
@@ -0,0 +1,26 @@
+package uzum.trelloapp.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "roles")
+public class Role {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", unique = true)
+    protected Long id;
+
+    @Column(name = "name", unique = true, nullable = true)
+    private String name;
+
+}
