cf5d603bb30376f64f7f9c4fdbb9d7e8e43d8619
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-23 21:44:15 +0500

    Task checkList+item+comments+members added

---
+++ src/main/java/uzum/trelloapp/entity/TaskCheckListItem.java
@@ -0,0 +1,28 @@
+package uzum.trelloapp.entity;
+
+import lombok.*;
+import org.hibernate.annotations.Type;
+import uzum.trelloapp.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "task_check_list_item")
+public class TaskCheckListItem extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_check_list_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private TaskCheckList taskCheckList;
+    @Column(name = "task_check_list_id")
+    private Integer taskCheckListId;
+
+    @Column(name = "is_finished", columnDefinition = "NUMERIC default 0")
+    @Type(type = "org.hibernate.type.NumericBooleanType")
+    protected boolean isFinished = false;
+
+}
---
+++ src/main/java/uzum/trelloapp/entity/TaskMembers.java
@@ -0,0 +1,29 @@
+package uzum.trelloapp.entity;
+
+import lombok.*;
+import uzum.trelloapp.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "task_members")
+public class TaskMembers extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "task_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Task task;
+    @Column(name = "task_id")
+    private Integer taskId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User user;
+    @Column(name = "user_id")
+    private Integer userId;
+
+}
