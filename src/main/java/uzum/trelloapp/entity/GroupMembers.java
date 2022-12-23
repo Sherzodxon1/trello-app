7ad349e6a892ce242af978bf332ffff2b78ea359
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-23 21:41:19 +0500

    GroupMembers added

---
+++ src/main/java/uzum/trelloapp/entity/GroupMembers.java
@@ -0,0 +1,29 @@
+package uzum.trelloapp.entity;
+
+import lombok.*;
+import uzum.trelloapp.base.BaseEntity;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "group_members")
+public class GroupMembers extends BaseEntity {
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "group_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private Group group;
+    @Column(name = "group_id")
+    private Integer groupId;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User user;
+    @Column(name = "user_id")
+    private Integer userId;
+
+}
