0d0fe5571686cb83478ee8f27e9b971c0b688946
bahromasrorkulov <bahrom142214@gmail.com>
2022-12-23 21:19:44 +0500

    User+Group+Project+Task fixed

--- src/main/java/uzum/trelloapp/entity/Group.java
+++ src/main/java/uzum/trelloapp/entity/Group.java
@@ -1,21 +1,34 @@
 package uzum.trelloapp.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 import uzum.trelloapp.base.BaseEntity;
+import uzum.trelloapp.enums.GrPrType;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
 
 @Entity
 @Getter
 @Setter
+@ToString
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "groups")
 public class Group extends BaseEntity {
 
-    private Integer memberIndex;
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "owner_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User owner;
+    @Column(name = "owner_id")
+    private Integer ownerId;
+
+    @Enumerated(value = EnumType.STRING)
+    @Column(name = "gr_pr_type")
+    private GrPrType grPrType;
+
 }
